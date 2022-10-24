cdf08586e3a2d2a23bed81647bd5e2c65ed54aa9
ebirses <116559677+ebirses@users.noreply.github.com>
2022-10-24 18:20:23 +0300

    Update hipotenus.java

--- hipotenus.java
+++ hipotenus.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
-public class hipotenus {
+public class alanbulma {
     public static void main(String[] args) {
         //değişkenler
-        int a, b;
-        double c;
+        int a, b, c;
+        double alan, u;
         //kullanıcı verileri
         Scanner girdi = new Scanner(System.in);
         System.out.print("1. Kenarı Giriniz(cm): ");
         a = girdi.nextInt();
         System.out.print("2. Kenarı Giriniz(cm): ");
         b = girdi.nextInt();
+        System.out.print("3. Kenarı Giriniz(cm): ");
+        c = girdi.nextInt();
 
-        c = Math.sqrt((a*a) + (b*b));
-        System.out.println("Hipotenüs: " + c+" cm");
+
+        //u = Math.sqrt((a*a) + (b*b));
+        u = ((a+b+c)/2);
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+
+        System.out.println("Hipotenüs: " +alan+" cm");
     }
 }
